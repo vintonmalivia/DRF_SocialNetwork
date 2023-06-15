ffe2b94578ca5a04b2ba2cdfdc87cba735eeb420
Anton Shvechikhin <antonkamski@gmail.com>
2023-06-15 23:31:41 +0400

    Added signals

--- social_network/apps.py
+++ social_network/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SocialNetworkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social_network'
+
+    def ready(self):
+        import social_network.signals
