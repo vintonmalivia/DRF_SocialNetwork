2f0ec544e0adc34ae182af7da6fc034aee7cc028
Anton Shvechikhin <antonkamski@gmail.com>
2023-06-15 23:30:28 +0400

    Created models

--- social_network/models.py
+++ social_network/models.py
@@ -1,4 +1,42 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    sending_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.user.username} to {self.recipient.user.username}"
+
+
+class Dialogue(models.Model):
+    participants = models.ManyToManyField(Profile, related_name='dialogues')
+    messages = models.ManyToManyField(Message)
+
+    def __str__(self):
+        participant_names = ", ".join([participant.user.username for participant in self.participants.all()])
+        return f"Dialogue between {participant_names}"
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    friend = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='friends')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.user.username} is friends with {self.friend.user.username}"
